4700274326f3deb58431937b02203630a08b29fd
liqiang <ken.li@inspiry.cn>
2018-08-19 20:04:11 +0800

    Site updated: 2018-08-19 20:04:10

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [["/sw-register.js","75f1aad12ee46d4582e0518f666d5e15"]];
+var precacheConfig = [];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
